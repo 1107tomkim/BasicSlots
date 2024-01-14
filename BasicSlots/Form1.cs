c31b96b7c718f7f042f0a64978601fe5a5b2c31e
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2024-01-14 14:46:55 -0600

    IntUtil to generate random numbers.

--- BasicSlots/Form1.cs
+++ BasicSlots/Form1.cs
@@ -17,11 +17,39 @@ namespace BasicSlots
             InitializeComponent();
         }
 
+        // Declare each item of the photos
+        public static int p1;
+        public static int p2;
+        public static int p3;
+         
+        // Declaring variables for display
+        public static long credits = 100;
+        public static long total = 0;
+        public static int bet = 5;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Assigns each indivdual boxes with the respective img files.
             pictureBox1.Image = Image.FromFile("1.png");
             pictureBox2.Image = Image.FromFile("2.png");
             pictureBox3.Image = Image.FromFile("3.png");
+        }
+
+        // Random Number Generator
+        public static class IntUtil
+        {
+            private static Random random;
+
+            private static void Init()
+            {
+                if (random == null) random = new Random();
+            }
+
+            public static int Random(int mix, int max)
+            {
+                Init(); 
+                return random.Next(mix, max);
+            }
         }
     }
 }
