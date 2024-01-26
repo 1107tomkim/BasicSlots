7cf7e3040f450eee9a9648e00f7e1e7d6d585f4c
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2024-01-25 19:54:14 -0600

    Changing size of display boxes to mimic a Slot wheel

--- BasicSlots/Form1.Designer.cs
+++ BasicSlots/Form1.Designer.cs
@@ -50,27 +50,27 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(42, 106);
+            this.pictureBox1.Location = new System.Drawing.Point(42, 107);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(210, 177);
+            this.pictureBox1.Size = new System.Drawing.Size(210, 200);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(293, 106);
+            this.pictureBox2.Location = new System.Drawing.Point(251, 106);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(210, 177);
+            this.pictureBox2.Size = new System.Drawing.Size(210, 200);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(538, 106);
+            this.pictureBox3.Location = new System.Drawing.Point(460, 107);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(210, 177);
+            this.pictureBox3.Size = new System.Drawing.Size(210, 200);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
@@ -137,27 +137,27 @@
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(42, 312);
+            this.pictureBox4.Location = new System.Drawing.Point(42, 306);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(210, 177);
+            this.pictureBox4.Size = new System.Drawing.Size(210, 203);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 9;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(293, 312);
+            this.pictureBox5.Location = new System.Drawing.Point(251, 305);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(210, 177);
+            this.pictureBox5.Size = new System.Drawing.Size(210, 203);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 10;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(538, 312);
+            this.pictureBox6.Location = new System.Drawing.Point(460, 306);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(210, 177);
+            this.pictureBox6.Size = new System.Drawing.Size(210, 202);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 11;
             this.pictureBox6.TabStop = false;
